4cb72dca4f4a609cd19abb931a4eb21c0f60a3dd
James Bradbury <jamesbradbury93@gmail.com>
2019-11-13 15:57:36 +0000

    more safety for cli args

--- mosh.nim
+++ mosh.nim
@@ -1,32 +1,45 @@
-import memfiles, parseopt, mosh_utils
+import memfiles, parseopt, mosh_utils, os, system
+#-- CLI Args --#
+when declared(commandLineParams):
+    var cliArgs = commandLineParams()
 
-var inputFile : string
-var argCtr : int
+if len(cliArgs) < 2:
+    echo "You need to provide an input and output file!"
+    quit()
 
-#Command line arg: e.g. ./mosh ./whatever.txt
-for kind, key, value in getOpt():
-    case kind
-        of cmdArgument:
-            inputFile = key
-            break           #only allow one arg
+# Parse Arguments
+var args : seq[string]
+var parser = initOptParser(cliArgs)
+while true:
+    parser.next()
+    case parser.kind
+    of cmdEnd: break
+    of cmdShortOption, cmdLongOption:
+      if parser.val == "":
+        echo "Option: ", parser.key
+      else:
+        echo "Option and value: ", parser.key, ", ", parser.val
+    of cmdArgument:
+      args.add(parser.key)
 
-        of cmdLongOption, cmdShortOption:
-            discard
+var iFile: string = args[0]
+var oFile: string = args[1]
+if not fileExists(iFile):
+    echo "The input file does not exist."
+    quit()
 
-        of cmdEnd:
-            discard
-
+#-- Process input file > output file --#
 let 
-    data = openRawFile(inputFile)
+    data = openRawFile(iFile)
     dataMem = data.mem
     dataSize = data.size
 
     header = createHeader(uint32(dataSize))
 
 var outputFile : File
-discard outputFile.open("toy_output.wav", fmWrite)
+discard outputFile.open(oFile, fmWrite)
 
-#Write header
+#-- Write header --#
 for value in header.fields:
     when value is array:
         for arrayVal in value:
@@ -34,7 +47,7 @@ for value in header.fields:
     else:
         discard outputFile.writeBuffer(unsafeAddr(value), sizeof(value))
 
-#Write actual content
+#-- Write input data --#
 discard outputFile.writeBuffer(dataMem, (sizeof(uint8) * dataSize))
 
 outputFile.close()
