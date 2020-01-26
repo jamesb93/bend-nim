fd627c0fc185a3c6d83983b12bc104cae54ea5cd
James Bradbury <jamesbradbury93@gmail.com>
2020-01-26 12:41:44 +0000

    working markov implementation

--- markov.nim
+++ markov.nim
@@ -12,46 +12,47 @@ proc buildChain(inputData:seq[int], order:int): Table[seq[int], seq[int]] =
         var mem = inputData[i..i+order]
         var key:seq[int] = mem[0..order-1]
         var pair:seq[int] = @[mem[order]] 
-        echo key, pair
 
         if not markov.hasKey(key):
             markov[key] = pair
         else:
             markov[key].add(pair)
     
-    # Create a special key for the final key
-    var specialKey = someData[(len(someData)-order)..len(someData)]
-    markov[specialKey] = @["_END_"]
-    for key, value in markov:
-        echo(key, value)
     return markov
 
-proc generateFromChain(sChain:Table, iterations:int, order:int): void =
+
+proc generateFromChain(sChain:Table, iterations:int, order:int): seq[int] =
     # Setup a places to store the results
     var res:seq[int]
     
     # Get a random key from the data, rather than the chain
-    let 
-        randomPoint:int = rand(0..len(someData))
-        randomSlice:seq[int] = someData[randomPoint..randomPoint + (order-1)]
-    echo randomPoint
-    echo randomPoint+(order-1)
-    var previousStates:seq[int] = randomSlice
-
+    var
+        randomPoint:int = rand(0..len(someData)-order-1)
+        randomSlice:seq[int] = someData[randomPoint..randomPoint+(order-1)]
+        previousStates:seq[int] = randomSlice
 
     # Now loop around for some iterations
     for i in 0..iterations:
-         var nextState = sample(
-                sChain[previousStates]
-            )
-        if nextState = 
+        var sampleSelection:seq[int]
+
+        if sChain.hasKey(previousStates):
+            sampleSelection = sChain[previousStates]
+        else:
+            # New valid point
+            randomPoint = rand(0..len(someData)-order-1)
+            randomSlice = someData[randomPoint..randomPoint+(order-1)]
+            sampleSelection = sChain[randomSlice]
+            
+        var nextState = sample(sampleSelection)
+        
         
         var nextKey:seq[int] = previousStates[1..previousStates.len()-1]
         nextKey.add(nextState)
         previousStates = nextKey
         
         res.add(nextState)
-    echo res 
+    return res
+
 
 
 
@@ -60,7 +61,8 @@ proc generateFromChain(sChain:Table, iterations:int, order:int): void =
     
     
     
-var order = 2
-var iters = 3
+var order = 3
+var iters = 30
 var chain = buildChain(someData, order)
-generateFromChain(chain, iters, order)
+var resu = generateFromChain(chain, iters, order)
+echo resu
